321df79ef7810fe59ab6c7d3f8cb4e88570268d5
bishop255 <andresalejandro2001@gmail.com>
2025-09-06 00:30:34 -0400

    v-2
    Se agrego validaciones con java script a los html de sesion y  registro

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+const muestraMensaje = (event) => {
+            event.preventDefault(); // Evita el envío del formulario
+            const name = document.getElementById('name').value;
+            if (!name){
+                alert('Ingresa tu nombre');
+                console.log('Nombre no ingresado')
+                return;
+            }
+            document.getElementById('message').innerText = `¡Bienvenido, ${name}!`;
+            document.getElementById('form1').reset(); // Reinicia el formulario
+        };
